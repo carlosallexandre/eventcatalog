586e4d47ae8c9b9ab4550b49a28170289076f745
carlosallexandre <carlos_costarodrigues@outlook.com>
2024-09-02 12:12:10 -0300

    refactor: move build ci script under .github dir

--- .github/scripts/build-ci.js
+++ .github/scripts/build-ci.js
@@ -4,13 +4,14 @@
 import { join } from 'node:path';
 import fs from 'fs';
 import { execSync } from 'node:child_process';
-const __dirname = import.meta.dirname;
+
+const __dirname = process.cwd();
 
 const args = process.argv.slice(2);
 const catalog = args[0] || 'default';
 
-const catalogDir = join(__dirname, '../');
-const projectDIR = join(__dirname, `../examples/${catalog}`);
+const catalogDir = join(__dirname);
+const projectDIR = join(__dirname, `/examples/${catalog}`);
 
 fs.copyFileSync(join(projectDIR, 'eventcatalog.config.js'), join(catalogDir, 'eventcatalog.config.js'));
 
